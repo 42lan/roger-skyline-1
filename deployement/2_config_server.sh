ee5d9f8fbc58239d2fb17d4e29069838131cc1c6
Aslan MALSAGOV <amalsago@student.42.fr>
2019-01-03 19:51:33 +0100

    read -p "Copy the following line in file that open when you press [enter]"

--- deployement/2_config_server.sh
+++ deployement/2_config_server.sh
@@ -61,7 +61,7 @@ sudo cp /etc/fail2ban/jail.conf /etc/fail2ban/jail.local
 sudo sed -i "s/action \= \%\(action\_\)s/action \= \%\(action_mwl\)s/g" /etc/fail2ban/jail.local
 read -p "Please put enables = true, maxretry = 3, port = 2222 and logpath =/var/log/auth.log"
 sudo vi /etc/fail2ban/jail.local
-echo "Copy the following line in file that open when you press [enter]"
+read -p "Copy the following line in file that open when you press [enter]"
 printf "kern.info\t|/var/lib/psad/psadfifo\n"
 sudo vi /etc/syslog.conf
 sudo service syslog restart
