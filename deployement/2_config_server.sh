6ce04c6b893e9104792f0cf6214c7b05fc9d1ce1
4slan <amalsago@student.42.fr>
2019-01-03 18:16:15 +0100

    update

--- deployement/2_config_server.sh
+++ deployement/2_config_server.sh
@@ -10,7 +10,7 @@ echo "| forget to to umake me more intellegent). I'll need your help.         |"
 echo "| By the way, forgive my English, as my creator is not very good in     |"
 echo "| English. So I have heritatedit from him.                              |"
 echo "+-----------------------------------------------------------------------+"
-sudo apt-get -y install git iptables-persistent fail2ban psad &>/dev/null
+sudo apt-get -yqq install git iptables-persistent fail2ban psad &>/dev/null
 sudo git clone https://github.com/4slan/roger-skyline-1.git rs1 &>/dev/null
 read -p "Could you please configurate your interface as static (y/n) " -n 1
 case $REPLY in
@@ -36,7 +36,7 @@ done
 sudo sed -i "s/Port .*/Port ${PORT}/g" /etc/ssh/sshd_config
 sudo sed -i "s/PermitRootLogin.*/PermitRootLogin no/g" /etc/ssh/sshd_config
 sudo service sshd restart
-echo "Now you need to denerate an key on your Host machine and copy it as follow"
+echo "You need to generate an key on your Host machine and copy it as follow"
 printf "\tssh-keygen\n"
 printf "\tssh-copy-id $USER@$(hostname -I)-p $PORT\n"
 read -p "Do you confirm that you have copied your public key (y/n) " -n 1
@@ -51,17 +51,17 @@ case $REPLY in
 		;;
 esac
 echo
-echo "Now you are able to work on thid server through SSH from your machine which is great"
-sudo cp firewall /etc/init.d/
+echo "Now you are able to work through SSH from your machine which is great"
+sudo cp ~/rs1/deployement/firewall /etc/init.d/
 sudo cp /etc/fail2ban/jail.conf /etc/fail2ban/jail.local
 sudo sed -i "s/action \= \%\(action\_\)s/action \= \%\(action_mwl\)s/g" /etc/fail2ban/jail.local
-read -p "Please put enables = true, maxretry = 3, port = 2222 and logpath  = /var/log/auth.log"
-vi /etc/fail2ban/jail.local
-sudo echo "$IPT -A INPUT -j LOG" >> /etc/init.d/firewall
-sudo echo "$IPT -A FORWARD -j LOG" >> /etc/init.d/firewall
+read -p "Please put enables = true, maxretry = 3, port = 2222 and logpath =/var/log/auth.log"
+sudo vi /etc/fail2ban/jail.local
+sudo echo "\$IPT -A INPUT -j LOG" >> /etc/init.d/firewall
+sudo echo "\$IPT -A FORWARD -j LOG" >> /etc/init.d/firewall
 sudo echo "kern.info	|/var/lib/psad/psadfifo" > /etc/syslog.conf
 sudo service syslog restart
-sudo sed -i "s/HOSTNAME .*/HOSTNAME ${REPLY}/g" /etc/ssh/sshd_config
+sudo sed -i "s/HOSTNAME .*/HOSTNAME $(hostame)/g" /etc/ssh/sshd_config
 sudo sed -i "s/ENABLE_SYSLOG_FILE .*/ENABLE_SYSLOG_FILE Y;/g" /etc/ssh/sshd_config
 sudo sed -i "s/IPT_WRITE_FWDATA .*/IPT_WRITE_FWDATA Y;/g" /etc/ssh/sshd_config
 sudo sed -i "s/ENABLE_AUTO_IDS .*/ENABLE_AUTO_IDS Y;/g" /etc/ssh/sshd_config
@@ -69,9 +69,10 @@ sudo sed -i "s/AUTO_IDS_DANGER_LEVEL .*/AUTO_IDS_DANGER_LEVEL 1;/g" /etc/ssh/ssh
 sudo psad --sig-update
 sudo psad -R
 sudo psad -S
-sudo cp update_packages /etc/cron.d/
+sudo cp ~/rs1/deployement/update_packages /etc/cron.d/
 sudo chmod +x /etc/cron.d/update_packages
-sudo cp cron_integrity /etc/cron.d/cron_integrity
+sudo cp ~/rs1/deployement/cron_integrity /etc/cron.d/cron_integrity
 sudo chmod +x /etc/cron.d/cron_integrity
+sudo rm -rf ~/rs1
 sudo sh /etc/init.d/firewall
 sudo netfilter-persistent save
