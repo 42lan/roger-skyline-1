29909828336b7766088c9a68e0cd66bfd4c44cf8
Aslan MALSAGOV <amalsago@student.42.fr>
2019-01-03 19:18:29 +0100

    &>/dev/null removed

--- deployement/2_config_server.sh
+++ deployement/2_config_server.sh
@@ -10,8 +10,8 @@ echo "| forget to to umake me more intellegent). I'll need your help.         |"
 echo "| By the way, forgive my English, as my creator is not very good in     |"
 echo "| English. So I have heritatedit from him.                              |"
 echo "+-----------------------------------------------------------------------+"
-sudo apt-get -yqq install git iptables-persistent fail2ban psad &>/dev/null
-sudo git clone https://github.com/4slan/roger-skyline-1.git rs1 &>/dev/null
+sudo apt-get -yqq install git iptables-persistent fail2ban psad
+sudo git clone https://github.com/4slan/roger-skyline-1.git rs1
 read -p "Could you please configurate your interface as static (y/n) " -n 1
 case $REPLY in
 	[yY])
